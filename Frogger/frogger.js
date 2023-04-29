33c28736aa7606736059758359d632619ec233a2
KitPyles <pyles.kit@gmail.com>
2023-04-29 18:38:36 -0500

    Frogger: Continued building backend. Added images for frog, boat, & cars

--- Frogger/frogger.js
+++ Frogger/frogger.js
@@ -1,27 +1,38 @@
 const timeLeftDisplay = document.querySelector('#frogTimeLeft')
 const resultDisplay = document.querySelector('#frogResult')
 const startPauseButton = document.querySelector('#startPauseButton')
-const squares = document.querySelector('.grid div')
+const squares = document.querySelectorAll('.frogger-grid div')
+const logsLeft = document.querySelector('.log-left')
+const  logsRight = document.querySelector('.log-right')
+const carsLeft = document.querySelector('.car-left')
+const carsRight = document.querySelector('.car-right')
+const width = 9
+let currentIndex = 76
+let timerId
+let outcomeTimerId
+let currentTime = 20
 
 function moveFrog(e) {
+    squares[currentIndex].classList.remove('frog')
     switch (e.key) {
         case 'a':
         case 'ArrowLeft':
-            console.log('left')
+            if (currentIndex % width !== 0) currentIndex--
             break
         case 'w':
         case 'ArrowUp':
-            console.log('up')
+            if (currentIndex - width >= 0) currentIndex -= width
             break
         case 's':
         case 'ArrowDown':
-            console.log('down')
+            if (currentIndex + width < squares.length) currentIndex += width
             break
         case 'd':
         case 'ArrowRight':
-            console.log('right')
+            if (currentIndex % width < width - 1) currentIndex++
             break
     }
+    squares[currentIndex].classList.add('frog')
 }
 
-document.addEventListener('keyup',moveFrog)
+document.addEventListener('keyup', moveFrog)
