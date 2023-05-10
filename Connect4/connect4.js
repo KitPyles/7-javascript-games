10b31fd6916d96539f4e3d0f84df0b9cf1c88111
KitPyles <pyles.kit@gmail.com>
2023-05-09 20:49:31 -0500

    Connect4: Finished game!!!

--- Connect4/connect4.js
+++ Connect4/connect4.js
@@ -4,79 +4,94 @@ document.addEventListener('DOMContentLoaded', () => {
     const displayCurrentPlayer = document.querySelector('#currentPlayer')
     let currentPlayer = 1
     const takenSq = document.querySelectorAll('.taken')
-    let gameOver = false
+    let gameOver = false;
 
     const winningArrays = [
-        [0, 1, 2, 3],
-        [41, 40, 39, 38],
-        [7, 8, 9, 10],
-        [34, 33, 32, 31],
-        [14, 15, 16, 17],
-        [27, 26, 25, 24],
-        [21, 22, 23, 24],
-        [20, 19, 18, 17],
-        [28, 29, 30, 31],
-        [13, 12, 11, 10],
-        [6, 5, 4, 3],
-        [0, 7, 14, 21],
-        [41, 34, 27, 20],
-        [1, 8, 15, 22],
-        [40, 33, 26, 19],
-        [2, 9, 16, 23],
-        [39, 32, 35, 18],
-        [3, 10, 17, 24],
-        [38, 31, 24, 17],
-        [4, 11, 18, 25],
-        [37, 30, 23, 16],
-        [5, 12, 19, 26],
-        [36, 29, 22, 15],
-        [6, 13, 20, 27],
-        [35, 28, 21, 14],
-        [0, 8, 16, 24],
-        [41, 33, 25, 17],
-        [7, 15, 23, 31],
-        [34, 26, 18, 10],
-        [14, 22, 30, 38],
-        [27, 19, 11, 3],
-        [35, 29, 23, 17],
-        [6, 12, 18, 24],
-        [28, 22, 16, 10],
-        [13, 19, 25, 31],
-        [21, 15, 9, 3],
-        [20, 26, 32, 38],
-        [36, 30, 24, 18],
-        [5, 11, 17, 23],
-        [37, 31, 25, 19],
-        [4, 10, 16, 22],
-        [2, 10, 18, 26],
-        [39, 31, 23, 15],
-        [1, 9, 17, 25],
-        [40, 32, 24, 16],
-        [9, 17, 25, 33],
-        [8, 16, 24, 32],
-        [11, 17, 23, 29],
-        [12, 18, 24, 30],
-        [1, 2, 3, 4],
-        [5, 4, 3, 2],
-        [8, 9, 10, 11],
-        [12, 11, 10, 9],
-        [15, 16, 17, 18],
-        [19, 18, 17, 16],
-        [22, 23, 24, 25],
-        [26, 25, 24, 23],
-        [29, 30, 31, 32],
-        [33, 32, 31, 30],
-        [36, 37, 38, 39],
-        [40, 39, 38, 37],
-        [7, 14, 21, 28],
-        [8, 15, 22, 29],
-        [9, 16, 23, 30],
-        [10, 17, 24, 31],
-        [11, 18, 25, 32],
-        [12, 19, 26, 33],
-        [13, 20, 27, 34]
+        [0,1,2,3],
+        [0,7,14,21],
+        [0,8,16,24],
+        [1,2,3,4],
+        [1,8,15,22],
+        [1,9,17,25],
+        [2,3,4,5],
+        [2,9,18,26],
+        [2,10,18,26],
+        [3,4,5,6],
+        [3,9,15,21],
+        [3,10,17,24],
+        [3,11,19,27],
+        [7,8,9,10],
+        [7,14,21,24],
+        [7,15,23,31],
+        [8,9,10,11],
+        [8,15,22,29],
+        [8,16,24,32],
+        [9,10,11,12],
+        [9,16,23,30],
+        [9,17,25,33],
+        [10,11,12,13],
+        [10,16,22,28],
+        [10,17,24,31],
+        [10,18,26,34],
+        [14,15,16,17],
+        [14,21,28,35],
+        [14,22,30,38],
+        [15,16,17,18],
+        [15,22,29,36],
+        [15,23,31,39],
+        [16,17,18,19],
+        [16,23,30,37],
+        [16,24,32,40],
+        [17,18,19,20],
+        [17,23,29,35],
+        [17,24,31,38],
+        [17,25,33,41],
+        [21,22,23,24],
+        [22,23,24,25],
+        [23,24,25,26],
+        [24,25,26,27],
+        [28,29,30,31],
+        [29,30,31,32],
+        [30,31,32,33],
+        [31,32,33,34],
+        [35,36,37,38],
+        [36,37,38,39],
+        [37,38,39,40],
+        [38,39,40,41]
     ]
 
+    if (!gameOver) {
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].addEventListener('click', handleClick);
+        }
+    } else if (gameOver) {
+        setGameOver();
+    }
+    function handleClick() {
+        const square = event.target
+        const id = square.id
+
+        if (gameOver) return;
+
+        if (square.classList.contains('taken')) {
+            alert('This square is already taken. Please, try again.')
+            // if the square below your current square is taken, you can fill it
+        } else if (squares[parseInt(id) + 7].classList.contains('taken')) {
+            square.classList.add('taken')
+            if (currentPlayer === 1) {
+                square.classList.add('player1')
+                currentPlayer = 2
+            } else {
+                square.classList.add('player2')
+                currentPlayer = 1
+            }
+            displayCurrentPlayer.innerHTML = currentPlayer
+            // if the user selects in unavailable square
+        } else {
+            alert('That is not a valid choice. Please, try again.')
+        }
+        checkBoard()
+    }
     function checkBoard() {
         for (let y = 0; y < winningArrays.length; y++) {
             const square1 = squares[winningArrays[y][0]]
@@ -96,46 +111,26 @@ document.addEventListener('DOMContentLoaded', () => {
             if (s11 && s21 && s31 && s41) {
                 result.innerHTML = 'Player 1 wins!';
                 gameOver = true;
-                break;
+                return;
             } else if (s12 && s22 && s32 && s42) {
                 result.innerHTML = 'Player 2 wins!';
                 gameOver = true;
-                break;
+                return;
+            } else if (squares.length - takenSq.length === 0) {
+                result.innerHTML = 'Draw!';
+                gameOver = true;
+                return;
             }
-        }
-
-        // if (currentPlayer === 1) {
-        //     console.log ('Player 1')
-        // } else {
-        //     console.log ('Player 2')
-        // }
-    }
-
-    for (let i = 0, len = squares.length; i < len; i++) {
-        const square = squares[i]
-        function handleClick() {
-            // if the square below your current square is taken, you can fill it
-            if (square.classList.contains('taken')) {
-                alert('This square is already taken. Please, try again.')
-            } else if (squares[i + 7].classList.contains('taken')) {
-                square.classList.add('taken')
-                if (currentPlayer === 1) {
-                    square.classList.add('player1')
-                    currentPlayer = 2
-                } else {
-                    square.classList.add('player2')
-                    currentPlayer = 1
-                }
-                displayCurrentPlayer.innerHTML = currentPlayer
-            } else {
-                alert('That is not a valid choice. Please, try again.')
-            }
-            checkBoard()
         }
         if (gameOver) {
-            square.removeEventListener('click', handleClick)
-        } else if (!gameOver) {
-            square.addEventListener('click', handleClick)
+            setGameOver();
+        }
+    }
+
+    function setGameOver(message) {
+        result.innerHTML = message;
+        for (let i = 0; i < squares.length; i++) {
+            squares[i].removeEventListener('click', handleClick)
         }
     }
 })
