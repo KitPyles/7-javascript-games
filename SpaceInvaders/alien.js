8a3652de7ea9c554beff8eff09372899e90e86d4
KitPyles <pyles.kit@gmail.com>
2023-06-05 10:17:22 -0500

    SpaceInvaders: Tutorial is completed. Troubleshooting moving score from <context> to innerHTML via DOM for consistency across arcade.

--- SpaceInvaders/alien.js
+++ SpaceInvaders/alien.js
@@ -1,3 +1,7 @@
+// DOM items
+const resultsDisplay = document.getElementById('.results');
+const scoreDisplay = document.getElementById('.score');
+
 // board
 let tileSize = 32;
 let rows = 16;
@@ -35,6 +39,13 @@ let alienCols = 3;
 let alienCount = 0; // # of aliens to defeat
 let alienVelocityX = 1; // alien movement speed
 
+// bullets
+let bulletArray= [];
+let bulletVelocityY = -10; // bullet move speed
+
+let score = 0;
+let gameOver = false;
+
 window.onload = function () {
     board = document.getElementById("board");
     let bgImages = [
@@ -70,11 +81,16 @@ window.onload = function () {
 
     requestAnimationFrame(update);
     document.addEventListener('keydown', moveShip);
-
+    document.addEventListener('keyup',shoot);
 }
 
 function update() {
     requestAnimationFrame(update);
+
+    if (gameOver) {
+        return;
+    }
+
     context.clearRect(0,0, board.width, board.height);
     // ship
     context.drawImage(shipImg, ship.x, ship.y, ship.width, ship.height);
@@ -84,13 +100,63 @@ function update() {
         if (alien.alive) {
             alien.x += alienVelocityX;
             // if alien touches border, switch direction
-
+            if (alien.x + alien.width >= board.width || alien.x <= 0) {
+                alienVelocityX *= -1;
+                alien.x += alienVelocityX * 2;
+                // move aliens 1 row
+                for (let j = 0; j < alienArray.length; j++) {
+                    alienArray[j].y += alien.height;
+                }
+            }
             context.drawImage(alienImg, alien.x, alien.y, alien.width, alien.height);
+
+            if (alien.y >= ship.y) {
+                gameOver = true;
+                resultsDisplay.innerHTML = `GAME OVER!! You earned ${score} points!`
+            }
         }
     }
+    // bullets
+    for (let i = 0; i < bulletArray.length; i++) {
+        let bullet = bulletArray[i];
+        bullet.y += bulletVelocityY;
+        context.fillStyle = "white";
+        context.fillRect(bullet.x, bullet.y, bullet.width, bullet.height);
+
+        // bullet collision
+        for (let j = 0; j < alienArray.length; j++) {
+            let alien = alienArray[j];
+            if (!bullet.used && alien.alive && detectCollision(bullet, alien)) {
+                bullet.used = true;
+                alien.alive = false;
+                alienCount--;
+                score += 100;
+            }
+        }
+    }
+    // clear bullets
+    while (bulletArray.length > 0 && (bulletArray[0].used || bulletArray[0].y < 0)) {
+        bulletArray.shift();
+    }
+
+    // next level
+    if (alienCount === 0) {
+        alienCols = Math.min(alienCols + 1, columns/2 - 2);
+        alienRows = Math.min(alienRows + 1, rows - 4);
+        alienVelocityX += 0.2; //speed up aliens
+        alienArray = [];
+        bulletArray = [];
+        createAliens();
+    }
+
+    // score
+    context.fillStyle = "white";
+    context.font = "16px courier";
+    context.fillText(score.toString(), 5, 20);
 }
 
 function moveShip(e) {
+    if (gameOver) return;
     if ((e.code === 'ArrowLeft' || e.keyCode === 65) && ship.x -shipVelocityX >= 0) {
         ship.x -= shipVelocityX;
     } else if ((e.code === 'ArrowRight' || e.keyCode === 68) && ship.x + shipVelocityX + shipWidth <= board.width) {
@@ -114,6 +180,31 @@ function createAliens() {
     }
     alienCount = alienArray.length;
 }
+
+function shoot(e) {
+    if (gameOver) return;
+    if (e.code === "Space") {
+        // shoot
+        let bullet = {
+            x: ship.x + shipWidth * 15/32,
+            y: ship.y,
+            width: tileSize/8,
+            height: tileSize/2,
+            used: false
+        }
+        bulletArray.push(bullet);
+    }
+}
+
+function detectCollision(a,b) {
+    return a.x < b.x + b.width &&  // a's top left corner doesn't reach b's top left corner
+        a.x +a.width > b.x &&        // a's top right corner passes b's top left corner
+        a.y < b.y + b.height &&     // a's top left corner doesn't reach b's bottom left corner
+        a.y + a.height > b.y;         // a's bottom left corner passes b's top left corner
+}
+
+
+
 
 
 
